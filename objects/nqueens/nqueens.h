1673c1e0835bf4c00e48f9c3c32dde6d4e144fd8
Nikhil Ragde <nragde@apple.com>
2018-04-30 16:36:02 -0700

    Close to final clean up, and commenting

--- objects/nqueens/nqueens.h
+++ objects/nqueens/nqueens.h
@@ -11,21 +11,14 @@ class nqueens {
         void generate_boards(int N);
     private:
         static const int COLS = 8;
+        int _find_queen(const int arr[][COLS], int row, int N);
+        void _zero_fill_row(int arr[][COLS], int row, int N);
         void _zero_fill(int arr[][COLS], int N);
+        bool _check_square(const int arr[][COLS], int r, int c, int N);
         bool _validate_solution(const int arr[][COLS], int N);
         void _print_solution(const int arr[][COLS], int N);
-        void _temp_fill(int arr[][COLS]);
-        bool _check_square(const int arr[][COLS], int r, int c, int N);
-        void _find_solutions(int arr[][COLS], int N);
-        void _zero_fill_row(int arr[][COLS], int row, int N);
-        int _find_queen(const int arr[][COLS], int row, int N);
-        
-        //void _move_up(int& r, int N);
-        //void _move_left(int& c, int N);
-        //void _move_down(int originalr, int originalc, int& r, int& c);
-        //void _move_diagonally(int arr[][COLS], int& r, int& c, int N);
-        //bool _validate_square(const int arr[][COLS], int N, int& sum);
         void _print_array(const int arr[][COLS], int N);
+        void _temp_fill(int arr[][COLS]);
 };
 
 #endif
