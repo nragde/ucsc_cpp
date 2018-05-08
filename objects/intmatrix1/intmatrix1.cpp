c64064cb7c16b0239af72c76e446a1672ff02042
Nikhil Ragde <nragde@apple.com>
2018-05-08 14:49:17 -0700

    Final edits and submission zip for hw5

--- objects/intmatrix1/intmatrix1.cpp
+++ objects/intmatrix1/intmatrix1.cpp
@@ -9,15 +9,10 @@ file: nqueens.cpp
 All includes here
 -----------------------------------------------------------------*/
 #include "intmatrix1.h"
-#define DEBUG0 1
+//#define DEBUG0 1
 //#define DEBUGTEST 1
 
-
-
-//void intmatrix1::init(){
-//    
-//}
-//void intmatrix1::init(int row = 0, int col = 0, int val = 0){
+//void intmatrix1::init(int row = 0, int col = 0, int val = 0);
 void intmatrix1::init(int row, int col, int val){
     //Empty Matrix:
     //If there are no inputs, row & col will be (defaulted to) 0
@@ -196,7 +191,6 @@ intmatrix1 intmatrix1::mult(intmatrix1 that){
                     int that_val = that_col[ind];
                     int prod = this_val * that_val;
                     cur_num += prod;
-//                    cur_num += this_row[ind] * that_col[ind];
                 }
                 prod_arr.set_arr_val(r, cc, cur_num);
             }
@@ -227,72 +221,6 @@ void intmatrix1::print(const char* str){
     
 }
 
-
-bool intmatrix1::_valid_init_str(const char* arr_str){
-    int num_rows = 0;
-    bool first_row = true;
-    int num_cols = 0;
-    int cur_col_num = 0;
-    int cur_row_cols = 0;
-   // int first_row_cols = 0;
-    bool within_number = false;
-    
-    while(*arr_str){
-#ifdef DEBUG0
-        cout << *arr_str;
-#endif
-        //Check what the current character is
-        //Is it a space
-        if(*arr_str == ' '){
-            if(within_number){
-                within_number = false;
-                cur_col_num++;
-                //cur_col_num = 0;
-            }
-        }
-        //Is it a |
-        else if (*arr_str == '|'){
-            //The first row defines how many numbers are for "cols"
-            if(within_number){
-                within_number = false;
-                cur_col_num++;
-            }
-            if(first_row){
-                first_row = false;
-                num_cols = cur_col_num;
-            }
-            //Every row must have the same number of columns
-            if(!(num_cols == cur_col_num)){
-                return false;
-            }
-            cur_col_num = 0;
-            num_rows++;
-        }
-        //Is it a number?
-        else if (*arr_str >= '0' && *arr_str <= '9'){
-            within_number = true;
-            
-        }
-        //If it's anything else, not valid
-        else{
-            return false;
-        }
-        arr_str++;
-    }
-    //Tweak the column count if the last character is a number
-    if(within_number){
-        within_number = false;
-        cur_col_num++;
-    }
-    //Need to verify that the last row is also valid
-    if(!(num_cols == cur_col_num)){
-            return false;
-    }
-    else{
-        num_rows++;
-    }
-    return true;
-}
 
 int intmatrix1::_char_to_int(const char* int_str){
     return int(*int_str - '0');
@@ -422,290 +350,136 @@ bool intmatrix1::_insert_str_vals(const char* arr_str){
     return true;
 }
 
-#if 0
-void nqueens::generate_boards(int N){
-    //Make sure we're only going up to N=8 queens
-    if(N>COLS){
-        cout << "The board is too large!" << endl;
-        return;
-    }
-    
-    //Allocate memory for a fresh board
-    int (*board)[COLS] = new int[N][COLS];
-    //Zero fill the NxN board to start off at a good point
-    _zero_fill(board, N);
-#ifdef DEBUGTEST
-    //Fill with known solution
-     _temp_fill(board);    
-    //Check that the validatation is proper
-    bool is_valid = _validate_solution(board, N);
-    //Check that the print is proper
-    if(is_valid){
-        _print_solution(board, N);
-    }
-    return;
-#endif
-    //Loop through row by row.
-    int row = 0;
-    int col = 0;
-    bool can_place_here = false;
-    bool row_placed = false;
-    int total_solutions = 0;
-    while(row < N){
-        //Look for the first valid empty spot, by traversing columns
-        while(col < N){
-            //Check the current row,col pair
-            can_place_here = _check_square(board, row, col, N);
-            //If this is a valid spot to place a queen, do it
-            if(can_place_here){
-                board[row][col] = 1;
-                row_placed = true;
-                break;
-            }
-            //If not that spot, check the next column
-            col++;
-        }
-        //A queen was placed in this row, so move to the next row
-        if(row_placed){
-            row++;
-            col = 0;
-            row_placed = false;
-#ifdef DEBUG
-            cout << "GOOD!" << endl;
-#endif
-        }
-        else{
-            //There's no valid position for the queen on this current board!
-            //We need to back track to the previous column and try again
-            row--;
-            int last_queen_col = _find_queen(board, row, N);
-            _zero_fill_row(board, row, N);
-            col = last_queen_col + 1;
-#ifdef DEBUG
-            cout << "BAD!" << endl;
-#endif
-        }
-        
-#ifdef DEBUG
-        _print_array(board, N);
-#endif
-        //If we've found a solution, row == N
-        if(row == N){
-            bool is_solution = _validate_solution(board, N);
-            if(is_solution){
-                total_solutions++;
-                cout << "Solution #" << total_solutions << " for " << N << "x" << N << " Board:" << endl;
-                _print_solution(board, N);
-            }
-            //Now, we need to backtrack to the appropriate point to continue looking for solutions
-            //It's possible that the first P (where P is less than N-1) rows will be the same for mutiple solutions.
-            //Since we can't know that, we'll move up one row (which is the N-1 row) and keep going to the right.
-            //This should keep going until row 0, col N-1 has been tested. This is our "terminating condition".
-            row--;
-            int last_queen_col = _find_queen(board, row, N);
-            _zero_fill_row(board, row, N);
-            col = last_queen_col + 1;
-        }
-        //We've tried all possible combos! So break out of the outer for loop and wrap things up!
-        if(row == 0 && col == N){
-            break;
-        }        
-    }
-   
-    delete[] board;
-    cout << "------------------------" << endl;
-    cout << "Total Solutions for an " << N << "x" << N << " Board: " << total_solutions << endl;
-}
-
-//Return the position of a queen in a certain row
-int nqueens::_find_queen(const int arr[][COLS], int row, int N){
-    for(int c = 0; c < N; c++){
-        if(arr[row][c]){
-            return c;
-        }
-    }
-    //We should never really get here, but return the invalid coordinate of N if
-    //we accidentally check an empty row!
-    return N;
-    
-}
-
-//Zero out a row to "remove" a placed queen
-void nqueens::_zero_fill_row(int arr[][COLS], int row, int N){
-    for(int c = 0; c < N; c++){
-        arr[row][c] = 0;
-    }
-}
-
-//Clear out an entire NxN board (necessary after allocation). 
-//The point is to ensure that we have a known starting point, 
-//since the allocated space will likely include garbage values.
-void nqueens::_zero_fill(int arr[][COLS], int N){
-    //Zero out each row in the NxN square
-    for (int r = 0; r < N; r++) {
-        _zero_fill_row(arr, r, N);
-    }
-}
-
-//Check a potential square (row,col) to see if it's, at least currently,
-//a valid spot for a queen piece.
-bool nqueens::_check_square(const int arr[][COLS], int r, int c, int N){
-    //There's guaranteed to be just one non-zero element: the queen
-    //Thus, if we encounter any non-zero values in any of the 8 directions, 
-    //we can return false immediately because we know that's invalid.
-    
-    //Look upward
-    for(int up_row = r-1; up_row >=0; up_row--){
-        if(arr[up_row][c]){
-            return false;
-        }                    
-    }
-    //Look downward
-    for(int down_row = r+1; down_row < N; down_row++){
-        if(arr[down_row][c]){
-            return false;
-        }                    
-    }
-    //Look rightward
-    for(int cc = c+1; cc < N; cc++){
-        if(arr[r][cc]){
-            return false;
-        }                    
-    }
-    //Look leftward
-    for(int cc = c-1; cc >=0; cc--){
-        if(arr[r][cc]){
-            return false;
-        }                    
-    }
-    //Diagonals, upward
-    //To the right
-    int cr = c+1;
-    //To the left
-    int cl = c-1;
-    for(int up_row = r-1; up_row >=0; up_row--){
-        //Up and to the right, if possible
-        if(cr < N){
-            if(arr[up_row][cr]){
-                return false;
-            }
-        }
-        //Up and to the left, if possible
-        if(cl >= 0){
-            if(arr[up_row][cl]){
-                return false;
-            }  
-        }
-        //Right one column
-        cr++;
-        //Left one column
-        cl--;
-    }
-    //Diagonals, downward
-    cr = c+1;
-    //To the left
-    cl = c-1;
-    for(int down_row = r+1; down_row < N; down_row++){
-        //Down and to the right, if possible
-        if(cr < N){
-            if(arr[down_row][cr]){
-                return false;
-            }
-        }
-        //Down and to the left, if possible
-        if(cl >= 0){
-            if(arr[down_row][cl]){
-                return false;
-            }  
-        }
-        //Right one column
-        cr++;
-        //Left one column
-        cl--;
-    }
-    return true;
-}
-
-
-bool nqueens::_validate_solution(const int arr[][COLS], int N){
-    //Traverse row by row
-    for(int r = 0; r < N; r++){
-        //Look column by column
-        for(int c = 0; c < N; c++){
-            //There's guaranteed to be just one non-zero element in a row: the queen
-            if(arr[r][c]){              
-                //Check that the current queen is in a valid spot
-                bool is_valid = _check_square(arr, r, c, N);
-                
-                //If the current queen is not valid, return false
-                if(!is_valid){
-                    return false;
-                }
-                //All the directions are clear, so we're done with this row!
-                //This is true because in valid solutions, 
-                //there can only be one queen per row.
-                break;
-            }
-        }
-    }
-    //We've checked all of the squares and haven't returned false, 
-    //so it's a valid solution!
-    return true;
-}
-
-//Print out the board: "-" for an empty spot, and "1" for a queen.
-void nqueens::_print_solution(const int arr[][COLS], int N){
-    //Print row by row, across columns.
-    //A queen should be printed as a "1". An empty square should be a "-".
-    for(int r = 0; r < N; r++){
-        for(int c = 0; c < N; c++){
-            int val = arr[r][c];
-            if(val){
-                cout << "1";
-            }
-            else{
-                cout << "-";
-            }
-        }
-        //New line at the end of a row
-        cout << endl;
-    }
-}
-
-//For debugging, print out the "raw" array (0 or 1). To be used to more easily 
-//see how the algorithm is working and how the queens are being placed in the 
-//empty squares.
-void nqueens::_print_array(const int arr[][COLS], int N){
-    //Print out a square, NxN array
-    for(int r = 0; r < N; r++){
-        for(int c = 0; c < N; c++){
-            //Put spaces between elements in a row. Don't worry about hanging space.
-            cout << arr[r][c] << " ";
-        }
-        //Put in a new line after a row is complete
-        cout << endl;
-    }
-}
-
-
-//A debug function. It fills a passed in array with a known 4x4 solution.
-void nqueens::_temp_fill(int arr[][COLS]){
-    //This is one of two solutions for a 4x4 board.
-    int good[4][4] = { {0,1,0,0}, 
-                       {0,0,0,1},
-                       {1,0,0,0},
-                       {0,0,1,0}};
-                        
-    //Place the queens in the proper spots (and empty out the right ones too).
-    for(int r = 0; r < 4; r++){
-        for(int c = 0; c < 4; c++){
-            arr[r][c] = good[r][c];
-        }
-    } 
-}
-#endif
 
 #if 0
 //OUTPUT GOES HERE:
-
+-------- Matrix a -----------
+Empty matrix
+-------- Matrix b -----------
+    0    0    0    0
+    0    0    0    0
+    0    0    0    0
+-------- Matrix c -----------
+Empty matrix
+-------- Matrix d -----------
+Empty matrix
+-------- Matrix e -----------
+    7    7    7    7    7    7    7    7    7    7
+    7    7    7    7    7    7    7    7    7    7
+    7    7    7    7    7    7    7    7    7    7
+-------- Matrix e -----------
+    1    2
+    3    4
+    5    6
+-------- Matrix g -----------
+    1    2
+    3    4
+    5    6
+-------- Matrix h -----------
+Empty matrix
+-------- Matrix a -----------
+    7    9   11
+   13   15   17
+-------- Matrix b -----------
+    6    8   10
+   12   14   16
+-------- matrix s -----------
+   13   17   21
+   25   29   33
+-------- matrix expected ans -----------
+   13   17   21
+   25   29   33
+-------- Matrix a -----------
+    1    2    3
+    4    5    6
+-------- Matrix b -----------
+    1    2
+-------- matrix s -----------
+Empty matrix
+-------- matrix expected ans -----------
+Empty matrix
+-------- Matrix a -----------
+    1    2    3
+-------- Matrix b -----------
+    2    1    3
+    3    3    2
+    4    1    2
+-------- matrix s -----------
+   20   10   13
+-------- matrix expected ans -----------
+   20   10   13
+----------------------------------
+-------- Matrix a -----------
+    3    4    2
+-------- Matrix b -----------
+   13    9    7   15
+    8    7    4    6
+    6    4    0    3
+-------- matrix s -----------
+   83   63   37   75
+-------- matrix expected ans -----------
+   83   63   37   75
+----------------------------------
+-------- Matrix a -----------
+    3
+-------- Matrix b -----------
+    5    2   11
+    9    4   14
+-------- matrix s -----------
+Empty matrix
+-------- matrix expected ans -----------
+Empty matrix
+----------------------------------
+-------- Matrix a -----------
+    3    9    0    2    2    9    5    2
+    0    2    2    1    9    6    6    8
+    7    5    6    1    4    9    8    9
+    3    3    2    9    2    1    7    4
+    1    9    0    1    2    9    5    2
+    4    2    0    3    7    3    9    1
+    5    9    0    6    6    7    8    2
+    9    3    4    6    8    4    9    1
+-------- Matrix b -----------
+    6    1    6    0    8    3    0    0
+    6    8    9    0    6    6    7    2
+    4    8    2    0    5    4    6    7
+    2    4    4    2    2    6    9    8
+    4    8    2    2    4    6    4    1
+    1    5    5    6    4    7    5    5
+    7    4    6    5    0    6    5    3
+    2    3    7    0    1    3    8    5
+-------- matrix s -----------
+  132  170  200   87  128  186  175  106
+  122  186  166   86   92  182  195  123
+  197  235  267  104  179  243  253  178
+  128  140  164   63   86  162  194  140
+  118  164  184   85  110  174  166   98
+  138  142  144   83   91  162  137   82
+  187  222  244  106  160  244  232  141
+  201  212  210   97  171  230  204  142
+-------- matrix expected ans -----------
+  132  170  200   87  128  186  175  106
+  122  186  166   86   92  182  195  123
+  197  235  267  104  179  243  253  178
+  128  140  164   63   86  162  194  140
+  118  164  184   85  110  174  166   98
+  138  142  144   83   91  162  137   82
+  187  222  244  106  160  244  232  141
+  201  212  210   97  171  230  204  142
+----------------------------------
+-------- Matrix a -----------
+    7    3
+    2    5
+    6    8
+    9    0
+-------- Matrix b -----------
+    8   14    0    3    1
+    7   11    5   91    3
+    8    4   19    5   57
+-------- matrix s -----------
+Empty matrix
+-------- matrix expected ans -----------
+Empty matrix
+----------------------------------
+        
 #endif
--- objects/intmatrix1/intmatrix1.h
+++ objects/intmatrix1/intmatrix1.h
@@ -8,7 +8,6 @@
 // Define the public and private methods for the object
 class intmatrix1 {
     public:
-        //void init();
         void init(int row = 0, int col = 0, int val = 0);
         void init(const char* arr_str);
         void fini();
@@ -17,6 +16,7 @@ class intmatrix1 {
         intmatrix1 add(intmatrix1 a);
         intmatrix1 mult(intmatrix1 a);
         void print(const char * str);
+        //The following are public functions to get/set private elements
         int get_rows();
         int get_cols();
         int get_arr_val(int row, int col);
@@ -27,19 +27,8 @@ class intmatrix1 {
         int _rows;
         int _cols;
         int* _arr;
-        bool _valid_init_str(const char* arr_str);
         bool _insert_str_vals(const char* arr_str);
         int _char_to_int(const char* int_str);
-        
-        
-//        int _find_queen(const int arr[][COLS], int row, int N);
-//        void _zero_fill_row(int arr[][COLS], int row, int N);
-//        void _zero_fill(int arr[][COLS], int N);
-//        bool _check_square(const int arr[][COLS], int r, int c, int N);
-//        bool _validate_solution(const int arr[][COLS], int N);
-//        void _print_solution(const int arr[][COLS], int N);
-//        void _print_array(const int arr[][COLS], int N);
-//        void _temp_fill(int arr[][COLS]);
 };
 
 #endif
