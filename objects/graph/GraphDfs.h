a8d3a295dd48f3156b37ffae9ba08bb1c803e1c2
Nikhil Ragde <nragde@apple.com>
2018-10-29 10:36:06 -0700

    Working DFS functions but still needs clean up

--- objects/graph/GraphDfs.h
+++ objects/graph/GraphDfs.h
@@ -19,6 +19,25 @@ All includes here
 Declaration of GraphDfs class
 -----------------------------------------------------------------*/
 class GraphDfs {
+public:
+    //We need to make changes to the Graph (temp val in node), so don't want it const
+    GraphDfs(Graph& gin, const char* tin, const string& sin, bool& cycle_in, int& work_in, vector<string>& ans_in);
+private:
+    Graph& _g;
+    const char* _t;
+    const string& _s;
+    bool& _cycle;
+    int& _work;
+    vector<string>& _ans;
+    bool _print;
+    
+    enum NodeColor { GREEN = 1, BLUE, RED,};
+    
+    //Private methods
+    void _perform_dfs();
+    void _dfs_r(int node_num);
+    void _set_to_unvisited();
+    bool _verify_dfs_topology();
 
 };
 
