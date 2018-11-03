a190081d619391910d421bce53b996485b291600
Nikhil Ragde <nragde@apple.com>
2018-11-03 15:39:12 -0700

    Add xcode project to repo, used to do native debugging in macOS

---
+++ xcode/ucsc_cpp/ucsc_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ucsc_cpp
+//
+//  Created by Nikhil Ragde on 10/12/18.
+//  Copyright © 2018 nragde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
